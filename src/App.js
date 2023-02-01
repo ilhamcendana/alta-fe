1849e1dfe000f5f643c2ffa8b58889292ff23951
cendanaaaa <ilham.cendana@antikode.com>
2023-02-01 23:46:55 +0700

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+/** Libs */
+
+/** Components */
+import Layout from './components/layout';
+
+/** Assets */
 import './App.css';
 
-function App() {
+export default function App() {
+  // State  
+  
+  // Func
+
+  // Handler    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        {/* Write your burgers code here */}
+      </Layout>
     </div>
   );
 }
-
-export default App;
